eb6c65fbbcea1e8662b7b9c0a07c254a6ff84ed2
Nezdoliy <nezdoliy.yura@gmail.com>
2020-04-18 06:58:36 +0300

    First commit

--- src/lesson1704/Lection170420.java
+++ src/lesson1704/Lection170420.java
@@ -1,3 +1,5 @@
+package lesson1704;
+
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
--- src/lesson1704/StreamExample.java
+++ src/lesson1704/StreamExample.java
@@ -1,3 +1,5 @@
+package lesson1704;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
